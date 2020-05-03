2eb53726694e885bdb1bb714f29628a01eb85cea
piinkyysuu <lthu1910@gmail.com>
2020-05-03 14:19:27 +0300

    initial commit

--- main.js
+++ main.js
@@ -10,8 +10,9 @@ $("input").keypress(function(event) {
 $("#button").on("click", function() {
     var input = $("input").val();
     if (input != "") {
+        var inputValue = "<li>"+ input +"<div class='toggleMenu'><div class='done'>Done</div><div class='delete'>Delete</div></div></li>" ;
         $("input").val("");
-        $("ul").append("<li>"+ input +"<div class='toggleMenu'><div class='done'>Done</div><div class='delete'>Delete</div></div></li>");
+        $(inputValue).appendTo("ul").hide().fadeIn("700");
     }
 });
 
@@ -19,8 +20,20 @@ $("ul").on("click", ".done", function(event) {
     $(this).parent().parent().fadeIn(600, function() {
         $(this).addClass("crossText");
     });
-    $(this).siblings().css({marginLeft:"70px"});
-    $(this).remove();
+    $(this).removeClass("done").addClass("undone");
+    $(this).text("Undone");
+
+    // $(this).siblings().css({marginLeft:"70px"});
+    // $(this).remove();
+    event.stopPropagation();
+})
+
+$("ul").on("click", ".undone", function(event) {
+    $(this).parent().parent().fadeIn(600, function() {
+        $(this).removeClass("crossText");
+    });
+    $(this).removeClass("undone").addClass("done");
+    $(this).text("Done");
     event.stopPropagation();
 })
 
